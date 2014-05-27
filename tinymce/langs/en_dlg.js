ec4822b6b3ad7e7d0c0bba0850a91432d159f247
agibson <art@embed.ly>
2014-05-27 12:52:15 -0400

    pull out strings from embed page for i18n

--- tinymce/langs/en_dlg.js
+++ tinymce/langs/en_dlg.js
@@ -2,7 +2,15 @@ tinyMCE.addI18n('en.embedly_dlg',{
 title:"Embed a URL",
 desc:"Embedly",
 url_field:"Enter a URL to embed",
-width_field:"Max Width (in pixels)",
-height_field:"Max Height (in pixels)",
-words_field:"Max word count"
+border_field:"Chromeless",
+background_field:"Dark Background",
+hover_title:"Hover Over Embed to Edit",
+close_button:"Close",
+no_key_title:"We're Sorry.",
+no_key_message:"An Embedly Key is required. You can sign up for a key."
+signup_link:"Go here."
+add_key_message:"If you already have an Embedly Key, please add it to the Embedly Plugin Settings.",
+options_title:"Options",
+cancel_button:"Cancel",
+post_button:"Add to Post"
 });
